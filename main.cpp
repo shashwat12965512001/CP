840e87ef345f92a118b3a8157909781452e44d06
Shashwat Srivastava <shashwat0805@protonmail.com>
2025-05-14 19:46:43 +0530

    1989: A. Catch the Coin (failed on test case 1)

--- main.cpp
+++ main.cpp
@@ -2,43 +2,62 @@
 
 using namespace std;
 
-namespace abc
-{
-    int a = 10;
-}
-
-namespace xyz
-{
-    int a = 5;
-}
-
 int main()
 {
-    // int a = INT_MAX;
-    // int b = INT_MIN;
-    // cout << a << endl;
-    // cout << b << endl;
-    // cout << (a + 1) << endl;
-    // cout << (a + 2) << endl;
-    // cout << (a + b) << endl;
+    ios::sync_with_stdio(false);
+    cout.tie(NULL);
+    cin.tie(NULL);
 
-    // double d = 1547.189722265;
-    // cout << d << endl; // showing double value upto 2 decimal places (default)
-    // // cout << fixed << setprecision(6) << d << endl; // showing double value upto 6 decimal places
-    // // cout << setprecision(6) << d << endl;
-    // // cout << setprecision(7) << d << endl;
-    // // cout << setprecision(11) << d << endl;
-    // cout << 1e2 << endl;  // 10^2
-    // cout << 1e-2 << endl; // 10^2
-    // cout << 1e-1 << endl; // 10^2
-
-    // cout << abc::a << endl;
-    // cout << xyz::a << endl;
-
-    // ios::sync_with_stdio(false);
-    // printf("5\n");
-    // cout << 4 << '\n';
-    // printf("3\n");
-    // cout << 2 << '\n';
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x, y, a, b, count;
+        cin >> x >> y;
+        a = b = count = 0;
+        while (count < 1000)
+        {
+            count++;
+            if (a < x)
+            {
+                if ((abs(x - a) != 1))
+                {
+                    a++;
+                }
+            }
+            else
+            {
+                if ((abs(x - a) != 1))
+                {
+                    a--;
+                }
+            }
+            if (b < y)
+            {
+                if ((abs(y - b) != 1))
+                {
+                    b++;
+                }
+            }
+            else
+            {
+                if ((abs(y - b) != 1))
+                {
+                    b--;
+                }
+            }
+            x--;
+            y--;
+            cout << "x: " << x << " | a: " << a << " | y: " << y << " | b: " << b << " \n";
+        }
+        if (x == a && y == b)
+        {
+            cout << "YES" << '\n';
+        }
+        else
+        {
+            cout << "NO" << '\n';
+        }
+    }
     return 0;
 }
