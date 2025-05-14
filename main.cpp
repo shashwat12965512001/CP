40d4e59c12cea10ef91a584365cca0b6a88e251f
Shashwat Srivastava <shashwat0805@protonmail.com>
2025-05-14 18:16:59 +0530

    Basic cpp :fast I/O floating number

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+namespace abc
+{
+    int a = 10;
+}
+
+namespace xyz
+{
+    int a = 5;
+}
+
+int main()
+{
+    // int a = INT_MAX;
+    // int b = INT_MIN;
+    // cout << a << endl;
+    // cout << b << endl;
+    // cout << (a + 1) << endl;
+    // cout << (a + 2) << endl;
+    // cout << (a + b) << endl;
+
+    // double d = 1547.189722265;
+    // cout << d << endl; // showing double value upto 2 decimal places (default)
+    // // cout << fixed << setprecision(6) << d << endl; // showing double value upto 6 decimal places
+    // // cout << setprecision(6) << d << endl;
+    // // cout << setprecision(7) << d << endl;
+    // // cout << setprecision(11) << d << endl;
+    // cout << 1e2 << endl;  // 10^2
+    // cout << 1e-2 << endl; // 10^2
+    // cout << 1e-1 << endl; // 10^2
+
+    // cout << abc::a << endl;
+    // cout << xyz::a << endl;
+
+    // ios::sync_with_stdio(false);
+    // printf("5\n");
+    // cout << 4 << '\n';
+    // printf("3\n");
+    // cout << 2 << '\n';
+    return 0;
+}
