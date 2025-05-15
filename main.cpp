86e5f944c00d0a5f6c8c059362b2de69b476b96d
Shashwat Srivastava <shashwat0805@protonmail.com>
2025-05-15 18:19:00 +0530

    1818: A. Politics (Submitted)

--- main.cpp
+++ main.cpp
@@ -2,163 +2,46 @@
 
 using namespace std;
 
-// Right Half Pyramid
-void rightHalfPyramid(int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < i; j++)
-        {
-            cout << "* ";
-        }
-        cout << '\n';
-    }
-}
-
-// Floyd's Triangle
-void floydsTriangle(int n)
-{
-    int count = 1;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < i; j++)
-        {
-            cout << count << " ";
-            count++;
-        }
-        cout << '\n';
-    }
-}
-
-// Inverted Right Half Pyramid
-void invertedRightHalfPyramid(int n)
-{
-    for (int i = n; i > 0; i--)
-    {
-        for (int j = 0; j < i; j++)
-        {
-            cout << "* ";
-        }
-        cout << '\n';
-    }
-}
-
-// Left Half Pyramid
-void leftHalfPyramid(int n)
-{
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 0; j < (n - i); j++)
-        {
-            cout << "  ";
-        }
-        for (int j = 0; j < (n - (n - i)); j++)
-        {
-            cout << "* ";
-        }
-        cout << '\n';
-    }
-}
-
-// Hollow Square
-void hollowSquare(int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        if (i == 0 || i == n - 1)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                cout << "* ";
-            }
-        }
-        else
-        {
-            cout << "* ";
-            for (int j = 1; j < n - 1; j++)
-            {
-                cout << "  ";
-            }
-            cout << "* ";
-        }
-        cout << '\n';
-    }
-}
-
-// Full Pyramid
-void fullPyramid(int n)
-{
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 0; j < n - i; j++)
-        {
-            cout << "  ";
-        }
-        for (int j = 0; j < (n - (n - i)); j++)
-        {
-            cout << "* ";
-        }
-        for (int j = 1; j < i; j++)
-        {
-            cout << "* ";
-        }
-        cout << '\n';
-    }
-}
-
-// Hourglass
-void invertedFullPyramid(int n)
-{
-    // First Half
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 0; j < (n - (n - i)); j++)
-        {
-            cout << "  ";
-        }
-        for (int j = 0; j < n - i; j++)
-        {
-            cout << "* ";
-        }
-        for (int j = 0; j < (n - i) - 1; j++)
-        {
-            cout << "* ";
-        }
-        cout << '\n';
-    }
-    // Second Half
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 0; j < n - i; j++)
-        {
-            cout << "  ";
-        }
-        for (int j = 0; j < i; j++)
-        {
-            cout << "* ";
-        }
-        for (int j = 0; j < i - 1; j++)
-        {
-            cout << "* ";
-        }
-        cout << '\n';
-    }
-}
-
 int main()
 {
     ios::sync_with_stdio(false);
     cout.tie(NULL);
     cin.tie(NULL);
 
-    int n = 5;
-    // rightHalfPyramid(n);
-    // floydsTriangle(n);
-    // invertedRightHalfPyramid(n);
-    // leftHalfPyramid(n);
-    // hollowSquare(n);
-    // fullPyramid(n);
-    // invertedFullPyramid(n);
-
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int n, k;
+        cin >> n >> k;
+        string opinions[n];
+        for (int j = 0; j < n; j++)
+        {
+            string single;
+            for (int s = 0; s < k; s++)
+            {
+                char ch;
+                cin >> ch;
+                single += ch;
+                switch (ch)
+                {
+                case '+':
+                    break;
+                case '-':
+                    break;
+                }
+            }
+            opinions[j] = single;
+        }
+        int count = 1;
+        for (int i = 1; i < n; i++)
+        {
+            if (opinions[i] == opinions[0])
+            {
+                count++;
+            }
+        }
+        cout << count << '\n';
+    }
     return 0;
 }
