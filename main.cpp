2c87edc56fbd819e7737e5c9ba3b3d31769aceca
Shashwat Srivastava <shashwat0805@protonmail.com>
2025-05-17 12:51:55 +0530

    removed all code

--- main.cpp
+++ main.cpp
@@ -8,29 +8,5 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int n;
-    cin >> n;
-    for (int i = 0; i < n; i++)
-    {
-        int size, count;
-        cin >> size;
-        int arr[size];
-        count = 0;
-        for (int j = 0; j < size; j++)
-        {
-            int val;
-            cin >> val;
-            arr[j] = val;
-        }
-        for (int j = 0; j < size - 1; j++)
-        {
-            if ((arr[j] % 2 == 0 && arr[j + 1] % 2 == 0) || (arr[j] % 2 != 0 && arr[j + 1] % 2 != 0))
-            {
-                count++;
-            }
-        }
-        cout << count << '\n';
-    }
-
     return 0;
 }
