7ec9fa252f3b2c41cc2bd5ff307481dabb9e0bd7
Shashwat Srivastava <shashwat0805@protonmail.com>
2025-05-19 19:31:46 +0530

    A - Full House (Solved)

--- main.cpp
+++ main.cpp
@@ -8,5 +8,54 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
+    int arr[5];
+    for (int i = 0; i < 5; i++)
+    {
+        int n;
+        cin >> n;
+        arr[i] = n;
+    }
+    int first, second;
+    first = second = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        int val = arr[i];
+        if (first == 0)
+        {
+            first = val;
+        }
+        else if (second == 0 && val != first)
+        {
+            second = val;
+        }
+    }
+    map<int, int> mp;
+    mp[first] = 0;
+    mp[second] = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        if (arr[i] == first)
+        {
+            mp[first]++;
+        }
+        else if (arr[i] == second)
+        {
+            mp[second]++;
+        }
+        else
+        {
+            cout << "No";
+            return 0;
+        }
+    }
+    if (mp[first] != 3 && mp[first] != 2)
+    {
+        cout << "No";
+    }
+    else
+    {
+        cout << "Yes";
+    }
+
     return 0;
 }
