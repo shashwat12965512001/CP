3f841df073c8acf7398b1b4c191cbe7368a1c68d
Shashwat Srivastava <shashwat0805@protonmail.com>
2025-05-24 10:22:23 +0530

    removed all code

--- main.cpp
+++ main.cpp
@@ -8,19 +8,5 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
-    long long n;
-    cin >> n;
-    long long fib[n];
-    fib[0] = 0;
-    if (n > 1)
-    {
-        fib[1] = 1;
-    }
-    for (int i = 2; i < n; i++)
-    {
-        fib[i] = fib[i - 1] + fib[i - 2];
-    }
-    cout << fib[n - 1];
-
     return 0;
 }
